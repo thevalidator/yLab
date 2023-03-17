a6380a223fc57e9d816034e822ec5e0899b0a653
thevalidator <the.validator@yandex.ru>
2023-03-17 22:32:48 +0300

    added skeleton for datedmap task

---
+++ third-task/src/main/java/ru/thevalidator/thirdtask/datedmap/DatedMapDemo.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.thirdtask.datedmap;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class DatedMapDemo {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ third-task/src/main/java/ru/thevalidator/thirdtask/datedmap/DatedMapImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.thirdtask.datedmap;
+
+import java.util.Date;
+import java.util.Set;
+
+
+public class DatedMapImpl implements DatedMap {
+
+    @Override
+    public void put(String key, String value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String get(String key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void remove(String key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Set<String> keySet() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Date getKeyLastInsertionDate(String key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
