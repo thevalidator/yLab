080217c7933aa773afaad1d47eaff076a72edf8a
thevalidator <the.validator@yandex.ru>
2023-03-18 11:57:43 +0300

    missing char Ё in check condition fixed

--- third-task/src/main/java/ru/thevalidator/thirdtask/transliterator/TransliteratorImpl.java
+++ third-task/src/main/java/ru/thevalidator/thirdtask/transliterator/TransliteratorImpl.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2023 thevalidator
  */
-
 package ru.thevalidator.thirdtask.transliterator;
 
 import java.io.BufferedReader;
@@ -9,21 +8,24 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class TransliteratorImpl implements Transliterator {
-    
-    public static final String DICT_FILE_NAME = "transliterator.dict";
-    private static final char MIN = 'А';
-    private static final char MAX = 'Я';
+
+    private static final String DICT_FILE_NAME = "transliterator.dict";
+
+    private final List<Character> alphabet;
     private final Map<Character, String> dictionary;
 
     public TransliteratorImpl() {
-        dictionary = initDictionary();
+        alphabet = new ArrayList<>();
+        dictionary = new HashMap<>();
+        initData(alphabet, dictionary);
     }
 
     @Override
@@ -31,10 +33,10 @@ public class TransliteratorImpl implements Transliterator {
         if (source == null) {
             return null;
         }
-        
+
         StringBuilder sb = new StringBuilder();
         for (char c : source.toCharArray()) {
-            if (c >= MIN && c <= MAX) {
+            if (alphabet.contains(c)) {
                 String replace = dictionary.get(c);
                 sb.append(replace);
             } else {
@@ -44,21 +46,21 @@ public class TransliteratorImpl implements Transliterator {
         return sb.toString();
     }
 
-    private Map<Character, String> initDictionary() {
-        Map<Character, String> dictionary = new HashMap<>();
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream(DICT_FILE_NAME);
+    private void initData(List<Character> alphabet, Map<Character, String> dictionary) {
+        try ( InputStream is = getClass().getClassLoader().getResourceAsStream(DICT_FILE_NAME);  
                 BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"))) {
-            
+
             String line;
             while ((line = br.readLine()) != null && !line.isBlank()) {
                 if (!validateLine(line)) {
                     throw new WrongLineFormatException(line);
                 }
                 Character c = line.charAt(0);
-                String v = line.substring(2).startsWith("-") ? "": line.substring(2);
+                String v = line.substring(2).startsWith("-") ? "" : line.substring(2);
+                alphabet.add(c);
                 dictionary.put(c, v);
             }
-            
+
         } catch (FileNotFoundException ex) {
             Logger.getLogger(TransliteratorImpl.class.getName()).log(Level.SEVERE, "ERROR FOUND", ex);
             System.exit(1);
@@ -66,8 +68,6 @@ public class TransliteratorImpl implements Transliterator {
             Logger.getLogger(TransliteratorImpl.class.getName()).log(Level.SEVERE, ex.getMessage());
             System.exit(1);
         }
-        
-        return dictionary;
     }
 
     //  simple input string validation
