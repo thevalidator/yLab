fa0013509357024ac566692840832e5dddb1364b
thevalidator <the.validator@yandex.ru>
2023-03-19 17:11:30 +0300

    Update OrgStructureDemo.java

--- third-task/src/main/java/ru/thevalidator/thirdtask/orgstructure/OrgStructureDemo.java
+++ third-task/src/main/java/ru/thevalidator/thirdtask/orgstructure/OrgStructureDemo.java
@@ -7,6 +7,7 @@ package ru.thevalidator.thirdtask.orgstructure;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import ru.thevalidator.thirdtask.orgstructure.entity.Employee;
 import ru.thevalidator.thirdtask.orgstructure.service.OrgStructureParser;
 import ru.thevalidator.thirdtask.orgstructure.service.impl.OrgStructureParserImpl;
 
@@ -18,16 +19,33 @@ public class OrgStructureDemo {
     public static void main(String[] args) throws IOException, URISyntaxException {
         
         OrgStructureDemo demo = new OrgStructureDemo();
-        File csvFile = demo.getCsvFile();
+        File csvFile = demo.getCsvFile("employees.csv");
+        //File csvFile = demo.getCsvFile("test.csv");
         
         OrgStructureParser parser = new OrgStructureParserImpl();
-        parser.parseStructure(csvFile);
+        Employee boss = parser.parseStructure(csvFile);
+        
+        printOrgStructure(boss);
         
     }
     
-    public File getCsvFile() throws URISyntaxException {
-        return new File(getClass().getClassLoader().getResource("employees.csv").toURI());
+    public File getCsvFile(String filename) throws URISyntaxException {
+        return new File(getClass().getClassLoader().getResource(filename).toURI());
     }
-
+    
+    public static void printOrgStructure(Employee e) {
+        printEmployee(e, "");
+    }
+    
+    private static void printEmployee(Employee e, String tab) {
+        System.out.println(tab 
+                + "ID = " + e.getId() 
+                + ", NAME = " + e.getName() 
+                + ", POS = " + e.getPosition());
+        
+        for (Employee s : e.getSubordinate()) {
+            printEmployee(s, tab + "\t");
+        }
+    }
     
 }
