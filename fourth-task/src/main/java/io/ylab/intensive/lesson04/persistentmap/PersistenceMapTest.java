2461e347182d6cc6a6bbfbf8d47ae44da7d60509
thevalidator <the.validator@yandex.ru>
2023-03-25 23:13:10 +0300

    Update PersistenceMapTest.java

--- fourth-task/src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
+++ fourth-task/src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
@@ -6,22 +6,59 @@ import javax.sql.DataSource;
 import io.ylab.intensive.lesson04.DbUtil;
 
 public class PersistenceMapTest {
-  public static void main(String[] args) throws SQLException {
-    DataSource dataSource = initDb();
-    PersistentMap persistentMap = new PersistentMapImpl(dataSource);
-    // Написать код демонстрации работы 
-  }
-  
-  public static DataSource initDb() throws SQLException {
-    String createMapTable = "" 
-                                + "drop table if exists persistent_map; " 
-                                + "CREATE TABLE if not exists persistent_map (\n"
-                                + "   map_name varchar,\n"
-                                + "   KEY varchar,\n"
-                                + "   value varchar\n"
-                                + ");";
-    DataSource dataSource = DbUtil.buildDataSource();
-    DbUtil.applyDdl(createMapTable, dataSource);
-    return dataSource;
-  }
+
+    public static void main(String[] args) throws SQLException {
+        
+        DataSource dataSource = initDb();
+        PersistentMap persistentMap = new PersistentMapImpl(dataSource);
+        
+        persistentMap.init(null);
+        
+        persistentMap.put(null, null);
+        System.out.println(" SIZE AFTER ADDING (NULL, NULL): " + persistentMap.getKeys().size());
+        System.out.println("             CONTAINS  NULL KEY: " + persistentMap.containsKey(null));
+        System.out.println();
+        
+        persistentMap.put(null, "some value");
+        System.out.println("SIZE AFTER ADDING (NULL, value): " + persistentMap.getKeys().size());
+        System.out.println("             CONTAINS  NULL KEY: " + persistentMap.containsKey(null));
+        System.out.println("                          VALUE: " + persistentMap.get(null));
+        System.out.println();
+        
+        persistentMap.put("some key", "some value2");
+        System.out.println(" SIZE AFTER ADDING (key, value): " + persistentMap.getKeys().size());
+        System.out.println("              CONTAINS      KEY: " + persistentMap.containsKey("some key"));
+        System.out.println("                          VALUE: " + persistentMap.get("some key"));
+        System.out.println();
+        
+        persistentMap.remove(null);
+        System.out.println("  SIZE  AFTER  REMOVE  NULL KEY: " + persistentMap.getKeys().size());
+        System.out.println("             CONTAINS  NULL KEY: " + persistentMap.containsKey(null));
+        System.out.println();
+        
+        System.out.println("===== NEW MAP CREATED ====");
+        persistentMap.init("new map");
+        
+        persistentMap.put(null, "some value");
+        System.out.println("SIZE AFTER ADDING (NULL, value): " + persistentMap.getKeys().size());
+        System.out.println("             CONTAINS  NULL KEY: " + persistentMap.containsKey(null));
+        System.out.println();
+        
+        persistentMap.clear();
+        System.out.println("    SIZE     AFTER     CLEARING: " + persistentMap.getKeys().size());
+        
+    }
+
+    public static DataSource initDb() throws SQLException {
+        String createMapTable = ""
+                + "drop table if exists persistent_map; "
+                + "CREATE TABLE if not exists persistent_map (\n"
+                + "   map_name varchar,\n"
+                + "   KEY varchar,\n"
+                + "   value varchar\n"
+                + ");";
+        DataSource dataSource = DbUtil.buildDataSource();
+        DbUtil.applyDdl(createMapTable, dataSource);
+        return dataSource;
+    }
 }
