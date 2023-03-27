ac40aa4848453e4278f9aa38180079f6f7022636
thevalidator <the.validator@yandex.ru>
2023-03-27 15:54:38 +0300

    Create DbHandler.java

---
+++ fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/service/DbHandler.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package io.ylab.intensive.lesson04.eventsourcing.db.service;
+
+import io.ylab.intensive.lesson04.eventsourcing.Person;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface DbHandler {
+
+    void savePerson(Person p);
+
+    void deletePerson(Person p);
+
+}
