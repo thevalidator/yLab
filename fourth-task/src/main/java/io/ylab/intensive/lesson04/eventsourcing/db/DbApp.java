af94a693cdd1d927c5f336f8e825e23a6a8ae7ef
thevalidator <the.validator@yandex.ru>
2023-03-28 13:19:47 +0300

    base logic done

--- fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
+++ fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
@@ -1,35 +1,92 @@
 package io.ylab.intensive.lesson04.eventsourcing.db;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 import io.ylab.intensive.lesson04.DbUtil;
 import io.ylab.intensive.lesson04.RabbitMQUtil;
+import io.ylab.intensive.lesson04.eventsourcing.communication.message.ActionType;
+import io.ylab.intensive.lesson04.eventsourcing.communication.message.Message;
+import io.ylab.intensive.lesson04.eventsourcing.communication.routing.Data;
+import io.ylab.intensive.lesson04.eventsourcing.db.service.DbHandler;
+import io.ylab.intensive.lesson04.eventsourcing.db.service.impl.DbHandlerImpl;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DbApp {
-  public static void main(String[] args) throws Exception {
-    DataSource dataSource = initDb();
-    ConnectionFactory connectionFactory = initMQ();
 
-    // тут пишем создание и запуск приложения работы с БД
-  }
-  
-  private static ConnectionFactory initMQ() throws Exception {
-    return RabbitMQUtil.buildConnectionFactory();
-  }
-  
-  private static DataSource initDb() throws SQLException {
-    String ddl = "" 
-                     + "drop table if exists person;" 
-                     + "create if not exists table person (\n"
-                     + "person_id bigint primary key,\n"
-                     + "first_name varchar,\n"
-                     + "last_name varchar,\n"
-                     + "middle_name varchar\n"
-                     + ")";
-    DataSource dataSource = DbUtil.buildDataSource();
-    DbUtil.applyDdl(ddl, dataSource);
-    return dataSource;
-  }
+    public static void main(String[] args) throws Exception {
+        
+        DataSource dataSource = initDb();
+        ObjectMapper mapper = new ObjectMapper();
+        
+        try (Connection dbConn = dataSource.getConnection();) {
+            dbConn.setAutoCommit(true);
+            DbHandler dbHandler = new DbHandlerImpl();
+            ConnectionFactory connectionFactory = initMQ();
+            com.rabbitmq.client.Connection brokerConn = connectionFactory.newConnection();
+            Channel channel = brokerConn.createChannel();
+            channel.exchangeDeclare(Data.EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+            String queueName = channel.queueDeclare().getQueue();
+            String bindingKey = "db.person.*";
+            channel.queueBind(queueName, Data.EXCHANGE_NAME, bindingKey);
+            int prefetchCount = 1;
+            channel.basicQos(prefetchCount);
+
+            Logger.getLogger(DbApp.class.getName()).log(Level.INFO, " [*] App started, waiting for messages...");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String incomingMessage = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                String logMsg = " [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + incomingMessage + "'";
+                Logger.getLogger(DbApp.class.getName()).log(Level.INFO, "{0} {1}", new Object[]{logMsg, Thread.currentThread().getName()});
+
+                try {
+                    Message message = mapper.readValue(incomingMessage, Message.class);
+                    if (message.getAction().equals(ActionType.SAVE)) {
+                        dbHandler.savePerson(message.getPerson());
+                    } else {
+                        dbHandler.deletePerson(message.getPerson());
+                    }
+
+                } catch (JsonProcessingException ex) {
+                    Logger.getLogger(DbApp.class.getName()).log(Level.SEVERE, ex.getMessage());
+                } finally {
+                    System.out.println(" [x] Done " + Thread.currentThread().getName());
+                }
+
+            };
+            boolean isHandled = true;
+            channel.basicConsume(queueName, isHandled, deliverCallback, consumerTag -> {
+            });
+
+        }
+
+
+    }
+
+    private static ConnectionFactory initMQ() throws Exception {
+        return RabbitMQUtil.buildConnectionFactory();
+    }
+
+    private static DataSource initDb() throws SQLException {
+        String ddl = ""
+                + "drop table if exists person;"
+                + "create table if not exists person (\n"
+                + "person_id bigint primary key,\n"
+                + "first_name varchar,\n"
+                + "last_name varchar,\n"
+                + "middle_name varchar\n"
+                + ")";
+        DataSource dataSource = DbUtil.buildDataSource();
+        DbUtil.applyDdl(ddl, dataSource);
+        return dataSource;
+    }
 }
