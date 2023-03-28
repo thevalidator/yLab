7a9d6712c04d44eaec7bfd76526fd76743d6604c
thevalidator <the.validator@yandex.ru>
2023-03-28 18:16:48 +0300

    Create Data.java

---
+++ fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/communication/routing/Data.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package io.ylab.intensive.lesson04.eventsourcing.communication.routing;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Data {
+
+    public static final String EXCHANGE_NAME = "test";
+    
+}
