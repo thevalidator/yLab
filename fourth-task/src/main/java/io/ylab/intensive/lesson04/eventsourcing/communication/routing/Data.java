eb816ead68160c32e954f99d630e6f924fd6fcbb
thevalidator <the.validator@yandex.ru>
2023-03-29 02:53:17 +0300

    Update Data.java

--- fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/communication/routing/Data.java
+++ fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/communication/routing/Data.java
@@ -8,7 +8,8 @@ package io.ylab.intensive.lesson04.eventsourcing.communication.routing;
  */
 public class Data {
 
-    public static final String EXCHANGE_NAME = "test";
+    public static final String EXCHANGE_NAME = "person";
+    public static final String QUEUE_NAME = "person_query";
     public static final String SAVE_ROUTING_KEY = "db.person.save";
     public static final String DELETE_ROUTING_KEY = "db.person.delete";
     
