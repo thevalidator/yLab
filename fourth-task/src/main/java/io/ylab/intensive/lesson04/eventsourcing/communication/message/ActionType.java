06ccdbe6c89c4688ae85670d22da2abc465e2753
thevalidator <the.validator@yandex.ru>
2023-03-28 12:56:18 +0300

    Create ActionType.java

---
+++ fourth-task/src/main/java/io/ylab/intensive/lesson04/eventsourcing/communication/message/ActionType.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package io.ylab.intensive.lesson04.eventsourcing.communication.message;
+
+/**
+ *
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public enum ActionType {
+    
+    DELETE,
+    SAVE;
+    
+}
