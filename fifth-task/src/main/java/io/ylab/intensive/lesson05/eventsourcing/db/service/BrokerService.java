ea9db2c72bb7f420942d0f957199ea9b3f7ce7aa
thevalidator <the.validator@yandex.ru>
2023-04-02 01:58:23 +0300

    Update BrokerService.java

--- fifth-task/src/main/java/io/ylab/intensive/lesson05/eventsourcing/db/service/BrokerService.java
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/eventsourcing/db/service/BrokerService.java
@@ -5,13 +5,14 @@
 package io.ylab.intensive.lesson05.eventsourcing.db.service;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author thevalidator <the.validator@yandex.ru>
  */
 public interface BrokerService {
     
-    void start() throws IOException;
+    void start() throws IOException, TimeoutException;
     
     void stop();
 
