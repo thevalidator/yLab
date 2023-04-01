c55e870979fb0f2acc20545290bca9edfe2dd198
thevalidator <the.validator@yandex.ru>
2023-04-02 01:16:14 +0300

    Create Query.java

---
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/eventsourcing/db/sql/Query.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package io.ylab.intensive.lesson05.eventsourcing.db.sql;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Query {
+    
+    public static String SAVE = "INSERT INTO person (person_id, first_name, last_name, middle_name) \n" +
+                                "VALUES(?, ?, ?, ?) \n" +
+                                "ON CONFLICT (person_id) DO \n" +
+                                "UPDATE SET first_name=?, last_name=?, middle_name=? where person.person_id=?;";
+    
+    public static String DELETE = "DELETE FROM person WHERE person_id=?;";
+
+}
