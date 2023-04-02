005b07f86196fc8fe258bc93e302409810748418
thevalidator <the.validator@yandex.ru>
2023-04-02 21:20:03 +0300

    Create Query.java

---
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/messagefilter/sql/Query.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package io.ylab.intensive.lesson05.messagefilter.sql;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Query {
+
+    public static final String DDL = ""
+            + "CREATE TABLE bad_word ( "
+            + "word varchar(50) NOT NULL, "
+            + "CONSTRAINT unq_tbl_word UNIQUE (word));";
+
+    public static final String IS_EXISTS = ""
+            + "SELECT exists "
+            + "("
+            + "SELECT 1 "
+            + "FROM bad_word "
+            + "WHERE word = ? "
+            + "LIMIT 1"
+            + ");";
+
+}
