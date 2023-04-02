b47c2f442450af370dd78f9d0ed65455388b740a
thevalidator <the.validator@yandex.ru>
2023-04-02 18:06:26 +0300

    Update MessageFilterApp.java

--- fifth-task/src/main/java/io/ylab/intensive/lesson05/messagefilter/MessageFilterApp.java
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/messagefilter/MessageFilterApp.java
@@ -3,8 +3,11 @@ package io.ylab.intensive.lesson05.messagefilter;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MessageFilterApp {
-  public static void main(String[] args) {
-    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
-    applicationContext.start();
-  }
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        applicationContext.start();
+
+    }
 }
