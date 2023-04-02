c25303c9e98d94c0f42ea6326e43fa2cd0b995dd
thevalidator <the.validator@yandex.ru>
2023-04-02 12:58:18 +0300

    Create MetaData.java

---
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/sqlquerybuilder/MetaData.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package io.ylab.intensive.lesson05.sqlquerybuilder;
+
+/**
+ *
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public enum MetaData {
+
+    COLUMN_NAME,
+    COLUMN_SIZE,
+    DATA_TYPE,
+    IS_NULLABLE,
+    IS_AUTOINCREMENT;
+
+}
