d5640c285432ec21743fae5a405790cf3c5a5616
thevalidator <the.validator@yandex.ru>
2023-04-04 02:13:56 +0300

    Update ConnectionManagerImpl.java

--- fifth-task/src/main/java/io/ylab/intensive/lesson05/messagefilter/service/impl/ConnectionManagerImpl.java
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/messagefilter/service/impl/ConnectionManagerImpl.java
@@ -3,35 +3,64 @@
  */
 package io.ylab.intensive.lesson05.messagefilter.service.impl;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
 import io.ylab.intensive.lesson05.messagefilter.service.ConnectionManager;
 import jakarta.annotation.PreDestroy;
-import java.sql.Connection;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConnectionManagerImpl implements ConnectionManager {
 
-    private final Connection connection;
+    private final java.sql.Connection dbConnection;
+    private final com.rabbitmq.client.Connection brokerConnection;
+    private final Channel inputChannel;
+    private final Channel outputChannel;
 
     @Autowired
-    public ConnectionManagerImpl(DataSource dataSource) throws SQLException {
-        connection = dataSource.getConnection();
+    public ConnectionManagerImpl(DataSource dataSource, 
+            ConnectionFactory connectionFactory,
+            @Value("${queue.in.name}") String inputQueue,
+            @Value("${queue.out.name}") String outputQueue) throws SQLException, IOException, TimeoutException {
+        dbConnection = dataSource.getConnection();
+        brokerConnection = connectionFactory.newConnection();
+        inputChannel = brokerConnection.createChannel();
+        outputChannel = brokerConnection.createChannel();
+        
     }
 
     @Override
-    public Connection getDbConnection() {
-        return connection;
+    public java.sql.Connection getDbConnection() {
+        return dbConnection;
+    }
+    
+    @Override
+    public com.rabbitmq.client.Connection getBrokerConnection() {
+        return brokerConnection;
+    }
+    
+    @Override
+    public Channel getInputChannel() {
+        return inputChannel;
+    }
+
+    @Override
+    public Channel getOutputChannel() {
+        return outputChannel;
     }
 
     @PreDestroy
     public void preDestroy() {
         try {
-            connection.close();
+            dbConnection.close();
         } catch (SQLException ex) {
             Logger.getLogger(ConnectionManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
