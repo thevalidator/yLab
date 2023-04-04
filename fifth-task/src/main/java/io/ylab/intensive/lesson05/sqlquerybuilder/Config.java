338651fec6b5eb1b7efc39a68b0a560bc4a6e10f
thevalidator <the.validator@yandex.ru>
2023-04-04 15:37:00 +0300

    Update Config.java

--- fifth-task/src/main/java/io/ylab/intensive/lesson05/sqlquerybuilder/Config.java
+++ fifth-task/src/main/java/io/ylab/intensive/lesson05/sqlquerybuilder/Config.java
@@ -10,15 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("io.ylab.intensive.lesson05.sqlquerybuilder")
 public class Config {
-  
-  @Bean
-  public DataSource dataSource() {
-    PGSimpleDataSource dataSource = new PGSimpleDataSource();
-    dataSource.setServerName("localhost");
-    dataSource.setUser("postgres");
-    dataSource.setPassword("postgres");
-    dataSource.setDatabaseName("postgres");
-    dataSource.setPortNumber(5432);
-    return dataSource;
-  }
+
+    @Bean
+    public DataSource dataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setServerName("localhost");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("postgres");
+        dataSource.setDatabaseName("postgres");
+        dataSource.setPortNumber(5432);
+        return dataSource;
+    }
 }
